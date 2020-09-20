88b1238bbdd95dfab4b59c4340c730cfce87727a
erickrv17 <erickrvrv1717@gmail.com>
2020-09-19 23:11:32 -0600

    building task component

---
+++ src/components/Task.js
@@ -0,0 +1,9 @@
+import React, { useContext } from "react";
+
+const Task = ({info}) => {
+    return(
+        <div className="task-Card">
+            <h4>{info}</h4>
+        </div>
+    )
+}
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TaskContext } from "../context/TasksContext";
 
 const TaskList = () => {
-  return <></>;
+  const {tasks} = useContext(TaskContext);
+  return(
+      <div className="tasksList">
+        {tasks.map((task) => (
+          <Task key={task}></Task>
+        ))}
+      </div>
+  ) 
 };
 
 export default TaskList;
